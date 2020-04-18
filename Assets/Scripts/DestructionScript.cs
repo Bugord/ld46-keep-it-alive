1978cd59d6482f5d8220e094221cdaff867aac40
maxds98 <max_d_s@tut.by>
2020-04-18 16:27:33 +0300

    added destruction

---
+++ Assets/Scripts/DestructionScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D), typeof(Joint2D))]
+public class DestructionScript : MonoBehaviour
+{
+    private Joint2D joint;
+
+    private void Start()
+    {
+        joint = GetComponent<Joint2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet")) 
+        {
+            joint.enabled = false;
+        }
+    }
+}
